c304c1392edb0067b2cadeb05834669f74562d52
Axely7 <axely7@hotmail.com>
2022-10-31 10:33:37 -0600

    express installed

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-import http from "http";
+import express from "express";
+const app = express();
 
-http
-  .createServer((req, res) => {
-    res.write("Hola mundo");
-    res.end();
-  })
-  .listen(8080);
+const port = 8080;
 
-console.log("Escuchando el puerto", 8080);
+// Servir contenido Estático
+
+app.use(express.static("public"));
+
+app.get("/hola-mundo", (req, res) => {
+  res.send("Hola mundo en su respectiva ruta");
+});
+
+app.get("*", (req, res) => {
+  res.send("404 page not found");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
