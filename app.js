db8be888938159a519ed2569ccdf58e10cdcc0b3
Axely7 <axely7@hotmail.com>
2022-11-05 16:33:55 -0600

    handlebar hbs installed, view created MVC implementing

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 import express from "express";
+import * as url from "url";
 const app = express();
-
 const port = 8080;
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
+app.set("view engine", "hbs");
 // Servir contenido Estático
-
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.get("/hola-mundo", (req, res) => {
   res.send("Hola mundo en su respectiva ruta");
 });
 
-app.get("*", (req, res) => {
-  res.send("404 page not found");
+app.get("/generic", (req, res) => {
+  res.sendFile(__dirname + "/public/generic.html");
+});
+
+app.get("/elements", (req, res) => {
+  res.sendFile(__dirname + "/public/elements.html");
 });
 
 app.listen(port, () => {
