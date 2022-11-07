f759b480de9d5c2d5c64d971a7c6a56c1ac729d9
Axely7 <axely7@hotmail.com>
2022-11-07 12:05:31 -0600

    creating componentes with handlebars

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 import express from "express";
 import * as url from "url";
+import hbs from "hbs";
+
 const app = express();
 const port = 8080;
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
+//handlers
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials", function (err) {});
+
 // Servir contenido Estático
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    nombre: "Axel Jimenez",
+    titulo: "Curso de Node",
+  });
+});
+
+app.get("/generic", (req, res) => {
+  res.render("generic", {
+    nombre: "Axel Jimenez",
+    titulo: "Curso de Node",
+  });
+});
+
+app.get("/elements", (req, res) => {
+  res.render("elements", {
+    nombre: "Axel Jimenez",
+    titulo: "Curso de Node",
+  });
 });
 
 app.get("/hola-mundo", (req, res) => {
